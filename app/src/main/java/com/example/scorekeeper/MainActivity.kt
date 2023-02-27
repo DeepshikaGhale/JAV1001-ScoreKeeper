230c95a37872e21f0281db112d31c49699f56b21
Deepshika Ghale <58839663+DeepshikaGhale@users.noreply.github.com>
2023-02-27 18:41:03 -0500

    Connect activity class and xml for radio buttons

--- app/src/main/java/com/example/scorekeeper/MainActivity.kt
+++ app/src/main/java/com/example/scorekeeper/MainActivity.kt
@@ -1,30 +1,59 @@
 package com.example.scorekeeper
 
-import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import android.widget.ImageButton
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var decrease : ImageButton
+    private lateinit var increase : ImageButton
+    private lateinit var teamARadioGroup: RadioGroup
+    private lateinit var teamBRadioGroup: RadioGroup
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-         decrease = findViewById(R.id.decrease_btn)
+        decrease = findViewById(R.id.decrease_btn)
+        increase = findViewById(R.id.increase_btn)
+        teamARadioGroup = findViewById(R.id.teamA_radio_grp)
+        teamBRadioGroup = findViewById(R.id.teamB_radio_grp)
 
-        decrease.setOnClickListener(){
-            printData()
-            print("This is message")
+        decrease.setOnClickListener() { decrease() }
+        increase.setOnClickListener() { increase }
+
+        teamARadioGroup.setOnCheckedChangeListener(
+            RadioGroup.OnCheckedChangeListener { _,
+                                                 checkedId ->
+                val radio: RadioButton = findViewById(checkedId)
+                Toast.makeText(this,"Score of team A is :"+
+                        " ${radio.text}",
+                    Toast.LENGTH_SHORT).show()
+            }
+        )
+
+        teamBRadioGroup.setOnCheckedChangeListener(
+            RadioGroup.OnCheckedChangeListener { _,
+                                                 checkedId ->
+                val radio: RadioButton = findViewById(checkedId)
+                Toast.makeText(this,"Score of team B is :"+
+                        " ${radio.text}",
+                    Toast.LENGTH_SHORT).show()
+            }
+        )
         }
 
+
+
+    private fun increase(){
+        Toast.makeText(this, "Score will be increased.", Toast.LENGTH_SHORT).show()
     }
 
-    fun printData(){
-        Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show()
+    private fun decrease(){
+        Toast.makeText(this, "Score will be decreased.", Toast.LENGTH_SHORT).show()
     }
 }
