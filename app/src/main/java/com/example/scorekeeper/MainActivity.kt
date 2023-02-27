317c8be43ef34b2d237f71e8c555c9d48a49ae58
Deepshika Ghale <58839663+DeepshikaGhale@users.noreply.github.com>
2023-02-27 17:28:50 -0500

    Fix layout margin for buttons

--- app/src/main/java/com/example/scorekeeper/MainActivity.kt
+++ app/src/main/java/com/example/scorekeeper/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.scorekeeper
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var decrease : ImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+         decrease = findViewById(R.id.decrease_btn)
+
+        decrease.setOnClickListener(){
+            printData()
+            print("This is message")
+        }
+
+    }
+
+    fun printData(){
+        Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show()
     }
 }
