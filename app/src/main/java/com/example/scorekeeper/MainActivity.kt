4a2e03fca54b285ccccc3e70c108ab1c8cd018c0
Deepshika Ghale <58839663+DeepshikaGhale@users.noreply.github.com>
2023-03-02 22:47:36 -0500

    Add events for buttons and fix layout ids

--- app/src/main/java/com/example/scorekeeper/MainActivity.kt
+++ app/src/main/java/com/example/scorekeeper/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.scorekeeper
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.RadioButton
 import android.widget.RadioGroup
@@ -10,57 +12,109 @@ import android.widget.Toast
 class MainActivity : AppCompatActivity() {
 
     //initialization
-    private lateinit var decrease : ImageButton
-    private lateinit var increase : ImageButton
-    private lateinit var teamARadioGroup: RadioGroup
-    private lateinit var teamBRadioGroup: RadioGroup
+    private lateinit var increaseBtnA : ImageButton
+    private lateinit var decreaseBtnA : ImageButton
+    private lateinit var increaseBtnB : ImageButton
+    private lateinit var decreaseBtnB : ImageButton
+    private lateinit var radioBtnGrp: RadioGroup
+    private lateinit var teamAScore : EditText
+    private lateinit var teamBScore : EditText
+
+    //stores score of team A and B
+    private var scoreA: Int = 0
+    private var scoreB: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         //connects xml and .kt file through view ids
-        decrease = findViewById(R.id.decrease_btn)
-        increase = findViewById(R.id.increase_btn)
-        teamARadioGroup = findViewById(R.id.teamA_radio_grp)
-        teamBRadioGroup = findViewById(R.id.teamB_radio_grp)
+        increaseBtnA = findViewById(R.id.teamA_increase_btn)
+        decreaseBtnA = findViewById(R.id.teamA_decrease_btn)
+        increaseBtnB = findViewById(R.id.teamB_increase_btn)
+        decreaseBtnB = findViewById(R.id.teamB_decrease_btn)
+        radioBtnGrp = findViewById(R.id.radio_btn_grp)
+        teamAScore = findViewById(R.id.teamA_score)
+        teamBScore = findViewById(R.id.teamB_score)
 
-        //when minus button of team A is pressed
-        decrease.setOnClickListener() { decrease() }
+        //get the selected radio button id and value
+        val selectedRadioButton: RadioButton = findViewById(radioBtnGrp.checkedRadioButtonId)
+        var scoreValue: Int = selectedRadioButton.text.toString().toInt()
 
-        //when plus button of team A is pressed
-        increase.setOnClickListener() { increase() }
+        //set the initial value for
+        teamAScore.setText(scoreA.toString())
+        teamBScore.setText(scoreB.toString())
 
-        //gives toast message with current radio button text for team A
-        teamARadioGroup.setOnCheckedChangeListener(
-            RadioGroup.OnCheckedChangeListener { _,
-                                                 checkedId ->
-                val radio: RadioButton = findViewById(checkedId)
-                Toast.makeText(this,"Score of team A is :"+
-                        " ${radio.text}",
-                    Toast.LENGTH_SHORT).show()
-            }
-        )
-
-        //gives toast message with current radio button text for team A
-        teamBRadioGroup.setOnCheckedChangeListener(
-            RadioGroup.OnCheckedChangeListener { _,
-                                                 checkedId ->
-                val radio: RadioButton = findViewById(checkedId)
-                Toast.makeText(this,"Score of team B is :"+
-                        " ${radio.text}",
-                    Toast.LENGTH_SHORT).show()
-            }
-        )
+        //increases team A score
+        increaseBtnA.setOnClickListener() {
+            teamAIncrease(scoreValue)
+            teamAScore.setText(scoreA.toString())
         }
 
+        //decreases team A score
+        decreaseBtnA.setOnClickListener() {
+            teamADecrease(scoreValue)
+            teamAScore.setText(scoreA.toString())
+        }
 
+        //increases team B score
+        increaseBtnB.setOnClickListener() {
+            teamBIncrease(scoreValue)
+            teamBScore.setText(scoreB.toString())
+        }
 
-    private fun increase(){
-        Toast.makeText(this, "Score will be increased.", Toast.LENGTH_SHORT).show()
+        //decreases team B score
+        decreaseBtnB.setOnClickListener() {
+            teamBDecrease(scoreValue)
+            teamBScore.setText(scoreB.toString())
+        }
+
+        //gets the currently selected radio button value
+        radioBtnGrp.setOnCheckedChangeListener(
+            RadioGroup.OnCheckedChangeListener { _,
+                                                 checkedId ->
+
+                val score: RadioButton = findViewById(checkedId)
+                scoreValue = score.text.toString().toInt()
+            }
+        )
     }
 
-    private fun decrease(){
-        Toast.makeText(this, "Score will be decreased.", Toast.LENGTH_SHORT).show()
+    //increases team A score
+    private fun teamAIncrease(score: Int){
+        scoreA += score
+        message('A')
+    }
+
+    //decreases team A score
+    private fun teamADecrease(score: Int){
+        if (scoreA >= score){
+            scoreA -= score
+            message('A')
+        }else{
+            Toast.makeText(this, "Please select different score value.", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    //increases team B score
+    private fun teamBIncrease(score: Int){
+        scoreB += score
+        message('B')
+    }
+
+    //decreases team B score
+    private fun teamBDecrease(score: Int){
+        if (scoreB >= score){
+            scoreB -= score
+            message('B')
+        }else{
+            Toast.makeText(this, "Please select different score value.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    //notifies user about score update
+    private fun message(team: Char){
+        Toast.makeText(this, "Score for team $team has been updates successfully.", Toast.LENGTH_SHORT).show()
     }
 }
